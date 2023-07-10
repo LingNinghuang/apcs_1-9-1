8b867f36947692d73ba6662147b4e8dd8c52ab0a
LingNinghuang <138959332+LingNinghuang@users.noreply.github.com>
2023-07-10 14:05:42 +0800

    Create Ex1-9-1.cpp

---
+++ Ex1-9-1.cpp
@@ -0,0 +1,11 @@
+//operators
+#include <stdio.h>
+int main() {
+    int sum1 = 11 + 24;
+    int sum2 = sum1 + 21;
+    int sum3 = sum2 + sum2;
+    printf("%d\n", sum1);
+    printf("%d\n", sum2);
+    printf("%d", sum3);
+    return 0;
+}
